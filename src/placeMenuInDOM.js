f283317dde79136bba3be986ac620f84f43fc4e0
Dexter Crago <jodieanddexter@gmail.com>
2024-01-25 09:20:30 -0700

    Adjust placeMenuInDOM parameters for locations

--- src/placeMenuInDOM.js
+++ src/placeMenuInDOM.js
@@ -1,20 +1,22 @@
 export default function placeMenuInDOM(vw) {
     const menuItems = document.querySelector('.menu-items');
 
-    menuItems.style.top = '';
-    menuItems.style.right = '';
+    menuItems.style.cssText = 'top: ; right: ;';
 
     if (vw > 682) {
         menuItems.style.top = '110px';
         menuItems.style.right = '40px';
-    }
+    };
 
-    if (vw > 389 && vw <= 682) {
+    if (vw <= 682 && vw > 626) {
+        menuItems.style.top = '182px';
+    };
+
+    if (vw <= 626 && vw > 398) {
         menuItems.style.top = '223px';
-    }
+    };
 
-    if (vw <= 389) {
+    if (vw <= 398) {
         menuItems.style.top = '264px';
-    }
-
+    };
 }
