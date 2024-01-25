2795ba05b409b6fd7c31c622a77bd52a486514d6
Dexter Crago <jodieanddexter@gmail.com>
2024-01-25 15:44:54 -0700

    Continue slide from indicated image
    Now if you click one of the indicators, the image that will display
    next will be the appropriate one in the order, instead of continuing
    on as if though you hadn't clicked anything.

--- src/displaySlideController.js
+++ src/displaySlideController.js
@@ -3,21 +3,32 @@ import displaySlide from "./displaySlide";
 import loopDisplaySlideController from "./loopDisplaySlideController";
 
 const images = getImages();
+let test = 0;
 
 export default function displaySlideController() {
-    let delay = 0;
 
-    for (let i = 0; i < images.length; i += 1) {
-        setTimeout(() => {
-            displaySlide(images[i]);
-        }, delay);
-        delay += 5000;
-    }
-
-    const loopDelay = images.length * 5000;
+    displaySlide(images[test]);
 
     setTimeout(() => {
+        const previousImage = document.querySelector('.image');
+
+        displaySlide(images[test]);
+
+        if (previousImage !== null) {
+            for (let j = 0; j < images.length; j += 1) {
+                if (previousImage.src === images[j].src) {
+                    test = j;
+                }
+            }
+        }
+    }, 5000);
+
+    setTimeout(() => {
+        test += 1;
+        if (test === images.length) {
+            test = 0;
+        }
         loopDisplaySlideController()
-    }, loopDelay);
+    }, 5000);
 
 }
