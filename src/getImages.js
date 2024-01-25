25b3f3af1ad5760962877eaf71790c6ab65329bb
Dexter Crago <jodieanddexter@gmail.com>
2024-01-25 16:44:51 -0700

    Add two more images

--- src/getImages.js
+++ src/getImages.js
@@ -1,12 +1,16 @@
 import Pikachu from './photos/pikachu.jpg';
 import Charmander from './photos/charmander.jpg';
 import Squirtle from './photos/squirtle.jpg';
+import Bulbasaur from './photos/bulbasaur.jpg';
+import Eevee from './photos/eevee.jpg';
 
 export default function getImages() {
     const imageSources = [
         Pikachu,
+        Eevee,
         Charmander,
         Squirtle,
+        Bulbasaur,
     ]
 
     const images = [];
